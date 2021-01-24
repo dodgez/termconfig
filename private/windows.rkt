4d476f7422d495ce1469ef56f96b035d221545f7
Zachary Dodge <zacharysdodge@gmail.com>
2021-01-23 23:20:50 -0800

    Fix c ffi types and check return values

--- private/windows.rkt
+++ private/windows.rkt
@@ -3,30 +3,35 @@
 (require ffi/unsafe
          ffi/unsafe/define)
 (define-ffi-definer define-c (ffi-lib #f))
+(define (check val where)
+  (when (zero? val)
+    (error where "failed: ~a ~a" val (GetLastError))))
+(define _HANDLE (_cpointer 'handle))
 
 ; HANDLE WINAPI GetStdHandle(_In_ DWORD nStdHandle);
-(define-c GetStdHandle (_fun _int -> _pointer))
+(define-c GetStdHandle (_fun _uint32 -> _HANDLE))
 ; BOOL WINAPI GetConsoleMode(_In_ HANDLE hConsoleHandle, _Out_ LPDWORD lpMode);
-(define-c GetConsoleMode (_fun _pointer _pointer -> _int))
+(define-c GetConsoleMode (_fun _HANDLE _pointer -> (r : _int) -> (check r 'GetConsoleMode)))
 ; BOOL WINAPI SetConsoleMode(_In_ HANDLE hConsoleHandle, _In_ DWORD dwMode);
-(define-c SetConsoleMode (_fun _pointer _int -> _int))
+(define-c SetConsoleMode (_fun _HANDLE _uint32 -> (r : _int) -> (check r 'SetConsoleMode)))
+; _Post_equals_last_error_ DWORD GetLastError();
+(define-c GetLastError (_fun -> _uint32))
 
-(define MAXMODE 639)
-(define LINE_INPT 2)
-(define VIRT_TERM_INPT 512)
-(define STDINPUT -10)
+(define RAW-FLAGS (- 639 4 2 1))
+(define VIRT-TERM-INPUT 512)
+(define STDINPUT 4294967286)
 (define lpMode (malloc 'atomic 8))
 
 (define (enable-raw)
   (define stdin (GetStdHandle STDINPUT))
   (GetConsoleMode stdin lpMode)
-  (define mode (bitwise-ior VIRT_TERM_INPT (bitwise-and (ptr-ref lpMode _int) (- MAXMODE LINE_INPT))))
+  (define mode (bitwise-ior VIRT-TERM-INPUT (bitwise-and (ptr-ref lpMode _uint32) RAW-FLAGS)))
   (SetConsoleMode stdin mode)
   (void))
 (define (disable-raw)
   (define stdin (GetStdHandle STDINPUT))
   (SetConsoleMode stdin (ptr-ref lpMode _int))
   (void))
-(define-syntax-rule (with-raw commands ...) (begin (enable-raw) commands ... (disable-raw) (void)))
+(define-syntax-rule (with-raw commands ...) (begin (enable-raw) commands ... (disable-raw)))
 
 (provide enable-raw disable-raw with-raw)
